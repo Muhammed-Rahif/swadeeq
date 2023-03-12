3aaf2a938f968cd1e7860bb809e93b17c75a66a0
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-03-12 22:00:27 +0530

    chore: update release.yml

--- scripts/versioning.js
+++ scripts/versioning.js
@@ -5,23 +5,10 @@ const androidBuildGradlePath = "android/app/build.gradle";
 const androidBuildGradle = fs.readFileSync(androidBuildGradlePath, "utf8");
 
 exports.preCommit = (props) => {
-  if (props.tag) {
-    const androidBuildGradleUpdated = androidBuildGradle.replace(
-      /versionName "v[\d\.]+"/g,
-      `versionName "${props.tag}"`
-    );
+  const androidBuildGradleUpdated = androidBuildGradle.replace(
+    /versionName "v[\d\.]+"/g,
+    `versionName "${props.tag}"`
+  );
 
-    fs.writeFileSync(androidBuildGradlePath, androidBuildGradleUpdated);
-  } else {
-    exec("git describe --tags", (error, stdout, stderr) => {
-      if (RegExp(/v[\d\.]+/g).test(stdout)) {
-        const androidBuildGradleUpdated = androidBuildGradle.replace(
-          /versionName "v[\d\.]+"/g,
-          `versionName "${stdout.replace("\n", "")}"`
-        );
-
-        fs.writeFileSync(androidBuildGradlePath, androidBuildGradleUpdated);
-      }
-    });
-  }
+  fs.writeFileSync(androidBuildGradlePath, androidBuildGradleUpdated);
 };
